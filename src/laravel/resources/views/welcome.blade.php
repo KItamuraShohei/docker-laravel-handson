db30e319f688da18badef82046876c1cb5697695
KitamuraShohei <kita.piece29@gmail.com>
2020-07-27 18:39:36 +0900

    さようなら追記

--- src/laravel/resources/views/welcome.blade.php
+++ src/laravel/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravelこんちには
+                    Laravelさようなら
                 </div>
 
                 <div class="links">
